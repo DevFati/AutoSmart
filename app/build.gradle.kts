07e7b67e203b41dc66a6d4666ebcc9773f03f9da
Fatima <fatmortcha@gmail.com>
2025-04-15 02:59:11 +0200

    Solved a bug with libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,6 @@ dependencies {
     implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.1")
 
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation (libs.material)
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,3 @@
-import org.gradle.kotlin.dsl.from
-
-
 pluginManagement {
     repositories {
         google {
@@ -14,20 +11,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven{ url= uri("https://jitpack.io")}
+        maven { url = uri("https://jitpack.io") }
+    }
 
-    }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
-
 rootProject.name = "AutoSmart"
 include(":app")
