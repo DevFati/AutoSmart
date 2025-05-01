fbfa70f63e4a475163cd7e36134c6a611f036691
Fatima <fatmortcha@gmail.com>
2025-05-01 17:47:38 +0200

    Now if i delete a vehicle, the maintenances related to it will be deleted too. And now when a user logs in, only the vehicles and their maintenances will be showed up.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,8 @@ dependencies {
 // Interceptor de logging (opcional, muy útil para depurar)
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 }
---
+++ app/src/main/java/com/example/autosmart/SuggestionsAdapter.java
@@ -0,0 +1,52 @@
+
+package com.example.autosmart;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.autosmart.MaintenanceSuggestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuggestionsAdapter
+        extends RecyclerView.Adapter<SuggestionsAdapter.VH> {
+
+    private final List<MaintenanceSuggestion> items = new ArrayList<>();
+
+    void submitList(List<MaintenanceSuggestion> list) {
+        items.clear();
+        items.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @NonNull @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup p, int v) {
+        View vew = LayoutInflater.from(p.getContext())
+                .inflate(android.R.layout.simple_list_item_2, p, false);
+        return new VH(vew);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH h, int pos) {
+        MaintenanceSuggestion s = items.get(pos);
+        h.tv1.setText(s.task);
+        h.tv2.setText("Frecuencia: "+s.frequency);
+    }
+
+    @Override public int getItemCount() { return items.size(); }
+
+    static class VH extends RecyclerView.ViewHolder {
+        TextView tv1, tv2;
+        VH(View iv) {
+            super(iv);
+            tv1 = iv.findViewById(android.R.id.text1);
+            tv2 = iv.findViewById(android.R.id.text2);
+        }
+    }
+}
